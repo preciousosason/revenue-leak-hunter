816747a9d28c28b8a6b482c1e5880f58ea78ad0f
preciousosason <preciousosason@gmail.com>
2026-09-12 14:03:56 +0100

    Connect blog to production backend

--- pages/blog/blog.js
+++ pages/blog/blog.js
@@ -9,6 +9,7 @@ const BLOG_API_URL =
     "http://192.168.57.31:8000/api/blog/";
     "http://192.168.57.31:8000/";
     "http://192.168.57.31:3000";
+    "https://revenue-leak-hunter-backend.onrender.com",
 
 
 /* =================================
